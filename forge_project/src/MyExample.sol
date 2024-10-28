32ea280fdd3b9ad507d647907fd6dc2ec0f1f3d7
Aggelos561 <angelos561@hotmail.com>
2024-10-28 18:37:54 +0200

    Testing with prank and deal.

--- forge_project/src/MyExample.sol
+++ forge_project/src/MyExample.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
+import {console} from "forge-std/Script.sol";
+
 contract MyExample {
 
     int private number;
@@ -19,5 +21,10 @@ contract MyExample {
 
     function getNumber() public view returns (int) {
         return number;
+    }
+
+    function getUserID() public payable  {
+        require(msg.value > 1, "Give me at least 1 Ether");
+        console.log("Called by ", msg.sender);
     }
 }
--- forge_project/test/MyExample.t.sol
+++ forge_project/test/MyExample.t.sol
@@ -27,4 +27,22 @@ contract MyExampleTest is Test {
     
         emit log_named_int("The value is: ", value);
     }
+
+    function testUsersBalance() public {
+
+        address user1 = address(1);
+        address user2 = address(2);
+
+        vm.deal(user1, 1 ether);
+        vm.deal(user2, 2 ether);
+
+        vm.prank(user1);
+        example.getUserID{value: 1 ether}();
+        
+        vm.prank(user2);
+        example.getUserID{value: 2 ether}();
+
+        console.log("User1 balance: ", user1.balance);
+        console.log("User2 balance: ", user2.balance);
+    }
 }
